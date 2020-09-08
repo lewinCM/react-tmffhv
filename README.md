dd443543a02d7593796faa167fbc2b530437a0c5
lewinCM <flowermoreno7@gmail.com>
2020-09-07 19:59:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tmffhv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tmffhv)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h2>hola mundo apenas comienzo!</h2>
+      <p>con este emocionante mundo de la programacion woa:)</p>
+      <p> de esto se trata, pactratuicar y mas practicas <span> vive la vida </span></p>
+    </div>
+    
+  );
+}
